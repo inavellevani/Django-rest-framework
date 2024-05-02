39e1e8fe3d1613742853cefb1e016184925d2627
inavellevani <levan.tsopurashvili.1@btu.edu.ge>
2024-05-02 19:20:12 +0400

    revert models file

--- products/models.py
+++ products/models.py
@@ -2,9 +2,13 @@ from django.db import models
 
 
 class Category(models.Model):
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
 
 
 class Product(models.Model):
